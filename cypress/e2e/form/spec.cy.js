b0b61ae3d3a94a697319b5b71d4084ec489449de
GPRNomade <guadapadinrojas@gmail.com>
2024-08-27 23:05:09 -0300

    refresh

--- cypress/e2e/form/spec.cy.js
+++
@@ -1,8 +0,0 @@
-describe("template spec", { testIsolation: false }, () => {
-  it("passes", () => {
-    cy.visit("https://demoqa.com");
-  });
-  it("got to other page", () => {
-    cy.get("h5").contains("Elements").click();
-  });
-});
--- cypress/e2e/form/test.cy.js
+++
@@ -1,8 +0,0 @@
-describe("test de formulario", () => {
-  it("Completar campo name", () => {
-    cy.visit("https://demoqa.com");
-    cy.get('h5').contains('Element').click();
-    cy.get('span')
-
-  });
-}); 
